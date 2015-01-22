bf44a773c78bf8b6e68a195f0afe0bc0e7bda6f4
Hannah Carney <hannahcarneyart@gmail.com>
2015-01-22 17:24:23 +0000

    Version two of layout complete

--- js/interface.js
+++ js/interface.js
@@ -4,17 +4,20 @@ var thermostat = new Thermostat();
 
 var updateColor = function() {
   $('#energyColor').addClass(thermostat.energyColor);
+  $('#PSM-title').addClass(thermostat.energyColor);
+
 };
 
 var removeColor = function() {
   $('#energyColor').removeClass();
+  $('#PSM-title').removeClass();
 };
 
 var updateTemperature = function() {
     removeColor();
     updateColor();
   $('#temperature').text(thermostat.temperature);
-  
+ 
 
 };
 
