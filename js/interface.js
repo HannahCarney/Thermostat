92afa6db95f68f905e8734d28d7bbbd9bcf55c74
Hannah Carney <hannahcarneyart@gmail.com>
2015-01-22 09:41:27 +0000

    Fixed bugs both reset and repeated code

--- js/interface.js
+++ js/interface.js
@@ -18,32 +18,3 @@ var updateTemperature = function() {
 
 };
 
-$(document).ready(function() {
-  updateTemperature();
-
-  $('.increaseTemp').on('click', function() {
-    thermostat.increaseTemperature(1);
-    updateTemperature();
-  });
-  
-  $('.decreaseTemp').on('click', function() {
-    thermostat.decreaseTemperature(1);
-    updateTemperature();
-  });
-
-  $('.reset').on('click', function() {
-    thermostat.reset();
-    updateTemperature();
-  });
-
-  $('.PSM-on').on('click', function() {
-    thermostat.powerSavingModeSwitch("on");
-    updateTemperature();
-  });
-
-  $('.PSM-off').on('click', function() {
-    thermostat.powerSavingModeSwitch("off");
-    updateTemperature();
-  });
-
-});
